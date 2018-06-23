d1a51a7b2e4c68ae47301ef8a2ce1ea8a250ba99
Nathan Breshears <nater778@live.com>
2018-06-23 13:13:22 -0700

    Initialized the game board and created print game board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class TicTacToe {
-	
-   int[][] board = new int[3][3];
-	
+
+   static char[][] board = new char[3][3];
+   
    public static void main(String[] args) 
    {
    
@@ -12,15 +12,16 @@ public class TicTacToe {
    
    while (input != 2)
    {
+	  System.out.println("Please make a choice (1 or 2): ");
       System.out.println("1. Start New Game");
       System.out.println("2. Quit");
-      System.out.println("Please make a choice (1 or 2): ");
+      
       input = kb.nextInt();
       
       
       if (input == 1) 
       {
-    	  StartNewGame();
+    	  startNewGame();
       }
       else if (input != 2) 
       {
@@ -34,9 +35,44 @@ public class TicTacToe {
    
    }
    
-   public static void StartNewGame() {
-	   initGame();
+   public static void startNewGame() 
+   {
+	 initGame();
+	 printBoard();
+	 
+	 
 	   
    }
    
+   public static void initGame()
+   {
+	   for (int i = 0; i < 3; i++)
+	   {
+		for (int j = 0; j < 3; j++)
+		{
+			board[i][j] = '-';
+		}
+	   }
+	   
+	  
+   }
+   
+   public static void printBoard()
+   {
+	   for (int i = 0; i < 3; i++)
+	   {
+		   for (int j = 0; j < 3; j++)
+		   {
+			 if (j != 2)
+			 {
+				 System.out.print(board[i][j] + " | ");
+			 }
+			 else 
+			 {
+				 System.out.println(board[i][j]);
+			 }
+		   }
+	   }
+	   
+   }
 }
